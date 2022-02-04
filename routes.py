7fbf8cf26cc0dc1ee47ee2614d958eeeef432848
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2022-02-04 15:16:32 +0200

    registeration and login first working version

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect
 from app import app
+import users
 
 @app.route("/")
 def index():
@@ -13,5 +14,22 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        session["username"] = username
-        return redirect("/")
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("login.html", error=True)
+
+@app.route("/register", methods=["get", "post"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        print(username, password)
+        if users.register(username, password):
+            return redirect("/")
+        else:
+            print("joo")
+            return render_template("register.html", error=True)
