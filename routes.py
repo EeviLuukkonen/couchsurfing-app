67f52bacd8dc110c6c8a443e1925fe14c2df68e4
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2022-02-04 11:09:05 +0200

    register and login html

--- routes.py
+++ routes.py
@@ -4,3 +4,14 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["get", "post"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        session["username"] = username
+        return redirect("/")
